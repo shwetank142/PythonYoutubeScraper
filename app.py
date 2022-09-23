e08a1ff96a6a7792f79b274c502a4aa942aa7787
shwetanksingh <shwetankkumar196@gmail.com>
2022-09-23 20:23:44 +0530

    new dependencies added

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def next_page():
     else:
         return render_template('index.html')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
 
